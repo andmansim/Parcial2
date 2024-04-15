c3d9c1e905d23077c44b1159e690797636edbd51
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2024-04-15 16:55:41 +0200

    ffff

--- figuritas.py
+++ figuritas.py
@@ -1,23 +1,34 @@
 import numpy as np
 import pandas as pd
-import meshpy.test as tet
+import meshpy.tet as tet
+from scipy.sparse import lil_matrix
 
 #creamos un dominio de 10 matrices de 0s de 10x10
 domain = np.zeros((10,10, 10))
 print(domain)
 
+#Paraview
 #creamos un csv con las coordenadas x, y,z y la presion
 df = pd.DataFrame({'x':[0,1], 'y':[0,1], 'z':[0,1], 'pressure':[100,150]})
 df.to_csv('data.csv', index=False)
 
-#calculo de gradientes
+#Funciones auxiliares 
 def tensor_deformacion(desplazamientos):
     # Implementa el cálculo del gradiente de desplazamientos
     # Este es un ejemplo simplificado
     return np.gradient(desplazamientos)
 
+#Preprocesamiento de datos de entrada
 points, facets = [ ... ], [ ... ]  # Define puntos y caras
 mesh_info = tet.MeshInfo()
 mesh_info.set_points(points)
 mesh_info.set_facets(facets)
 mesh = tet.build(mesh_info)
+
+#implementación de la función de forma
+def funcion_forma(punto, nodos):
+    # Implementa la función de forma
+    return np.array([ ... ])
+
+K_global = lil_matrix((nodos_totales, nodos_totales))
+# Suma las matrices locales de rigidez al K_global en los índices correctos
